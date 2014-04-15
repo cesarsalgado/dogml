7dfcea368c563e74dccd88bd14867f078454fe48
Cesar Salgado <cesarsvs@gmail.com>
2014-04-14 21:38:47 -0300

    Added DiffFramesClassifier class.

--- classification.py
+++ classification.py
@@ -43,6 +43,21 @@ class TwoFramesClassifier:
   def __str__(self):
     return self.clf.__str__()
 
+class DiffFramesClassifier:
+  def __init__(self, clf):
+    self.clf = clf
+  def diff(self, X):
+    n,d = X.shape
+    return cv2.absdiff(X[0:n-1,:], X[1:n,:])
+  def fit(self, X, y):
+    Z = self.diff(X)
+    self.clf.fit(Z, y[1:])
+  def predict(self, X2):
+    Z2 = self.diff(X2)
+    return self.clf.predict(Z2)
+  def __str__(self):
+    return self.clf.__str__()
+
 def original_order_train_test_fraction_split(train_fraction, data, labels):
   maxidx_train = int(train_fraction*len(labels))
   Xtrain = data[:maxidx_train,:]
