89ba10a38f0115caa2f8b1dd4173f6af206c5267
Cesar Salgado <cesarsvs@gmail.com>
2014-04-15 01:44:38 -0300

    Created superclasses to wrapper classes.
    Centered all the common behaviour to superclasses of the wrapper
    classes of the classification.py file.
    
                     WrapperClassifier
            _________________|_______________
            |                               |
            v                               v
    NormalizerClassifier      ConsecutiveFramesClassifier
                              ______________|______________
                              |                           |
                              V                           V
              ConcatenateTwoFramesClassifier      DiffFramesClassifier

--- classification.py
+++ classification.py
@@ -13,7 +13,13 @@ class GlobalNormalizer:
   def transform(self, X2):
     return (X2 - self.mean)/self.std
 
-class NormalizerClassifier:
+class WrapperClassifier:
+  def __init__(self):
+    pass
+  def __str__(self):
+    return "%s:\n%s"%(self.__class__.__name__, self.clf.__str__())
+
+class NormalizerClassifier(WrapperClassifier):
   def __init__(self, normalizer, clf):
     self.normalizer = normalizer
     self.clf = clf
@@ -25,38 +31,32 @@ class NormalizerClassifier:
   def predict(self, X2):
     Z2 = self.normalizer.transform(X2)
     return self.clf.predict(Z2)
-  def __str__(self):
-    return self.clf.__str__()
 
-class TwoFramesClassifier:
+class ConsecutiveFramesClassifier(WrapperClassifier):
   def __init__(self, clf):
     self.clf = clf
-  def merge(self, X):
+  def transform(self, X):
+    return
+  def fit(self, X, y):
+    Z = self.transform(X)
+    self.clf.fit(Z, y[1:])
+  def predict(self, X2):
+    Z2 = self.transform(X2)
+    return self.clf.predict(Z2)
+
+class ConcatenateTwoFramesClassifier(ConsecutiveFramesClassifier):
+  def __init__(self, clf):
+    ConsecutiveFramesClassifier.__init__(self, clf)
+  def transform(self, X):
     n,d = X.shape
     return np.hstack((X[0:n-1,:], X[1:n,:]))
-  def fit(self, X, y):
-    Z = self.merge(X)
-    self.clf.fit(Z, y[1:])
-  def predict(self, X2):
-    Z2 = self.merge(X2)
-    return self.clf.predict(Z2)
-  def __str__(self):
-    return self.clf.__str__()
 
-class DiffFramesClassifier:
+class DiffFramesClassifier(ConsecutiveFramesClassifier):
   def __init__(self, clf):
-    self.clf = clf
-  def diff(self, X):
+    ConsecutiveFramesClassifier.__init__(self, clf)
+  def transform(self, X):
     n,d = X.shape
     return cv2.absdiff(X[0:n-1,:], X[1:n,:])
-  def fit(self, X, y):
-    Z = self.diff(X)
-    self.clf.fit(Z, y[1:])
-  def predict(self, X2):
-    Z2 = self.diff(X2)
-    return self.clf.predict(Z2)
-  def __str__(self):
-    return self.clf.__str__()
 
 def original_order_train_test_fraction_split(train_fraction, data, labels):
   maxidx_train = int(train_fraction*len(labels))
