53cbc5aa8b023e58150c1bd39bb07bf14f046726
Cesar Salgado <cesarsvs@gmail.com>
2014-04-14 21:37:49 -0300

    Added TwoFramesClassifier class.

--- classification.py
+++ classification.py
@@ -28,6 +28,21 @@ class NormalizerClassifier:
   def __str__(self):
     return self.clf.__str__()
 
+class TwoFramesClassifier:
+  def __init__(self, clf):
+    self.clf = clf
+  def merge(self, X):
+    n,d = X.shape
+    return np.hstack((X[0:n-1,:], X[1:n,:]))
+  def fit(self, X, y):
+    Z = self.merge(X)
+    self.clf.fit(Z, y[1:])
+  def predict(self, X2):
+    Z2 = self.merge(X2)
+    return self.clf.predict(Z2)
+  def __str__(self):
+    return self.clf.__str__()
+
 def original_order_train_test_fraction_split(train_fraction, data, labels):
   maxidx_train = int(train_fraction*len(labels))
   Xtrain = data[:maxidx_train,:]
